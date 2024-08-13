4e8a05689990b7f5ca903a0a174bfa1b5adf97cb
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-08-13 00:08:52 -0400

    header-lourdes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,1 +1,21 @@
-//Lourdes
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/HeaderHome.css';
+
+const Header = () => {
+    return (
+        <div className="header-home">
+            <h1>Raining Offers For <span>Hot Summer!</span></h1>
+            <p>25% Off On All Products</p>
+            <button className="comprar-ahora-boton">
+                {/* Desplaza hasta el componente de ofertas */}
+                <a href="#content">SHOP NOW</a>
+            </button>
+            <button className="encontrar-mas-boton">
+                <Link to="/">FIND MORE</Link>
+            </button>
+        </div>
+    );
+};
+
+export default Header;
