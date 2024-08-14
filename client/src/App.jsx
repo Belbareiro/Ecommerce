2130bca8cdc67c2a8d48ceb4331ca708c8b678ab
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-08-14 07:49:19 -0400

    importar login y register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Products from './pages/PageSection/PageSection';
 import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 const App = () => {
   return (
@@ -10,7 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Products />} />
         <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
