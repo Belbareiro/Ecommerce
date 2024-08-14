40cc9a220b9f280006e9fca4e41689d1651d7eb5
Nicol Rotelaewe20 <166536745+nicolewe20@users.noreply.github.com>
2024-08-14 02:19:12 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Products from './pages/PageSection/PageSection';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Products />} />
         {/* Add more routes as needed */}
