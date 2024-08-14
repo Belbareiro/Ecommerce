400ea9bf5c878d81974f634d9e3528f247c5e876
Belen Bareiro <mbareiromereles@gmail.com>
2024-08-14 16:46:33 -0400

    cambios en productListPrincipal

--- client/src/pages/PageSection/PageSection.jsx
+++ client/src/pages/PageSection/PageSection.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../PageSection/PageSection.css';
-import icon from '../assets/icon.png';
-import photo1 from '../assets/photo1.jpg';
-import photo2 from '../assets/photo2.jpg';
-import photo3 from '../assets/photo3.jpg';
+import icon from '../assets/pageSection/icon.png';
+import photo1 from '../assets/pageSection/photo1.jpg';
+import photo2 from '../assets/pageSection/photo2.jpg';
+import photo3 from '../assets/pageSection/photo3.jpg';
 import Header from '../../components/Header';
-import ProductListPrincipal from '../ProductListPrincipal';
+import ProductListPrincipal from '../ProductListPrincipal/ProductListPrincipal';
 
 const PageSection = () => {
   const photos = [
     { id: 1, src: photo1, title: '20% Off on glasses for you', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', buttonText: 'SHOP NOW' },
-    { id: 2, src: photo2, title: 'Long-sleeved shirts: style and comfort for men.', description: 'Sed ac magna vel magna bibendum, egestas magna vel.', buttonText: 'SHOP NOW' },
+    { id: 2, src: photo2, title: 'Long-sleeved shirts: style and comfort for men', description: 'Sed ac magna vel magna bibendum, egestas magna vel.', buttonText: 'SHOP NOW' },
     { id: 3, src: photo3, title: 'Look now at the products we have for you', description: 'Vestibulum ante ipsum primis in faucibus orci luctus et.', buttonText: 'CHECK OUT' },
   ];
 
@@ -31,7 +31,7 @@ const PageSection = () => {
         {photos.map(photo => (
           <div className="photo" key={photo.id}>
             <img src={photo.src} alt={`Foto ${photo.id}`} />
-           <div className='containerTitle'> <h2 className="title">{photo.title}</h2></div>
+            <div className='containerTitle'> <h2 className="title">{photo.title}</h2></div>
             <p className="description">{photo.description}</p>
             <button className="button">{photo.buttonText}</button>
           </div>
---
+++ client/src/pages/ProductListPrincipal/ProductListPrincipal.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import '../ProductListPrincipal/ProductListPrincipal.css'
+import women1 from '../assets/women/women1.jpg';
+import women2 from '../assets/women/women2.jpg';
+import women3 from '../assets/women/women3.jpg';
+import men1 from '../assets/men/men1.jpg';
+import men2 from '../assets/men/men2.jpg';
+import men3 from '../assets/men/men3.jpg';
+import acc1 from '../assets/accessories/acc1.png';
+import acc2 from '../assets/accessories/acc2.jpg';
+import acc3 from '../assets/accessories/acc3.jpg';
+import acc4 from '../assets/accessories/acc4.jpg';
+
+
+const images = [
+  { src: women1, title: 'Elegant Dress', category: 'Women', link: '#women', price: 25.99 },
+  { src: women2, title: 'Casual Blouse', category: 'Women', link: '#women', price: 30.50 },
+  { src: women3, title: 'Summer Shorts', category: 'Women', link: '#women', price: 15.75 },
+  { src: men1, title: 'Classic Jeans', category: 'Men', link: '#men', price: 45.00 },
+  { src: men2, title: 'Sports Shoes', category: 'Men', link: '#men', price: 60.25 },
+  { src: men3, title: 'Blue Shoes', category: 'Men', link: '#men', price: 32.99 },
+  { src: acc1, title: 'Engagement Ring', category: 'Accessories', link: '#accessories', price: 12.50 },
+  { src: acc2, title: 'Glasses', category: 'Accessories', link: '#accessories', price: 8.99 },
+  { src: acc3, title: 'Sports Cap', category: 'Accessories', link: '#accessories', price: 20.00 },
+  { src: acc4, title: ' Watch', category: 'Accessories', link: '#accessories', price: 5.75 },
+];
+
+const ProducListPrincipal = () => {
+  return (
+    <div className="gallery">
+      {images.map((image, index) => (
+        <div className="item" key={index}>
+          <a href={image.link}>
+            <img src={image.src} alt={image.title} />
+            <div className="title">{image.title}</div>
+            <div className="price" >
+              ${image.price.toFixed(2)} {/* Formato de precio con dos decimales */} </div>
+            <div className="category">{image.category}</div>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default ProducListPrincipal;
