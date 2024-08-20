f5fd8a6cc64e1b3d16f8414dd61b071e02fb22cb
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-08-19 21:03:01 -0400

    actualizacion shop now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,24 @@ import Register from './pages/Register';
 import CargarProductos from './pages/CargarProductos';
 
 const App = () => {
+  const [cartCount, setCartCount] = useState(0);
+
+  const handleAddToCart = () => {
+    setCartCount(cartCount + 1);
+  };
+
   return (
     <div>
-      <Navbar/>
+      <Navbar cartCount={cartCount} />
       <Routes>
-        <Route path="/" element={<Products />} />
+        <Route path="/" element={<Products onAddToCart={handleAddToCart} />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-	<Route path="/products/new" element={<CargarProductos />} />
-    
+        <Route path="/products/new" element={<CargarProductos />} />
       </Routes>
     </div>
   );
 };
+
 
 export default App;
