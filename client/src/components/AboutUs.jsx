b104fd61226c03d57c4b5496dfc357ca1a8c0d19
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-08-28 01:36:33 -0400

    Update AboutUs

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import './AboutUs.css';
-import { link } from '../../../server/routes/products';
+import './AboutUs.css'; 
 
-const AboutUs = () => {
-    return (
+const AboutUs = ({ isOpen, onClose }) => {
+  if (!isOpen) return null;
+
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
         <section className="about-us">
-            <h2>About Us</h2>
-            <p>  Bienvenido a D'BCLAM, su destino único para lo último en moda. Ofrecemos una amplia gama de ropa, zapatos y accesorios, cuidadosamente seleccionados para mantenerte elegante y cómodo.
-                Nuestra misión es brindarle lo mejor en calidad y diseño, atendiendo a todas sus necesidades de moda, ya sea ropa casual, vestimenta de oficina o trajes de noche. Creemos en el poder de la buena moda y nos dedicamos a ayudarte a expresar tu estilo personal.                                                                                                                                                  </p>
-            <p>Nuestro equipo trabaja incansablemente para mantenerse a la vanguardia de las tendencias, asegurando que nuestra colección esté siempre fresca y relevante. Centrándonos en la sostenibilidad, también ofrecemos una selección de productos ecológicos que le permiten
-                comprar con confianza. Gracias por elegir D'BCLAM, donde la moda se encuentra con la pasión.                                                                                               </p>
+          <h2>Quiénes Somos</h2>
+          <p>
+            Bienvenido a D'BCLAM, su destino único para lo último en moda.
+            Ofrecemos una amplia gama de ropa, zapatos y accesorios,
+            cuidadosamente seleccionados para mantenerte elegante y cómodo.
+            Nuestra misión es brindarle lo mejor en calidad y diseño,
+            atendiendo a todas sus necesidades de moda, ya sea ropa casual,
+            vestimenta de oficina o trajes de noche.
+          </p>
+          <p>
+            Nuestro equipo trabaja incansablemente para mantenerse a la
+            vanguardia de las tendencias, asegurando que nuestra colección
+            esté siempre fresca y relevante. Centrándonos en la
+            sostenibilidad, también ofrecemos una selección de productos
+            ecológicos que le permiten comprar con confianza.
+          </p>
         </section>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
