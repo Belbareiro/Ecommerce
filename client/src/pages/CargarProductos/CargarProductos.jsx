5a51e6dae42b0e563e8aea0fe837cfdfcbb1de3e
Lourdes Valenzuela <126906933+LourdesValenzuela@users.noreply.github.com>
2024-08-28 02:43:49 -0400

    Update CargarProductos.css

--- client/src/pages/CargarProductos/CargarProductos.jsx
+++ client/src/pages/CargarProductos/CargarProductos.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import '../CargarProductos/CargarProductos.css'; // Asegúrate de que el archivo CSS esté presente para el estilo
+import '../CargarProductos/CargarProductos.css'; 
 
 const CargarProductos = ({ actualizarListaProductos }) => {
     const [nombre, setNombre] = useState("");
@@ -46,87 +46,82 @@ const CargarProductos = ({ actualizarListaProductos }) => {
             console.log("Algo falló", error.response ? error.response.data : error.message);
             setError(error.response ? error.response.data.message : "Error de conexión");
         }
-    }
-
+    };
 
     return (
-        <>
-            <div className="cargar-producto-container">
-                {showSuccessMessage && (
-                    <div>
-                        <p>¡Producto agregado exitosamente!</p>
-                    </div>
-                )}
-                {error && (
-                    <div>
-                        <p style={{ color: 'red' }}>{error}</p>
-                    </div>
-                )}
-                <form onSubmit={enviarFormularioProducto}>
-                    <div>
-                        <label htmlFor="nombre">Nombre:</label>
-                        <input
-                            type="text"
-                            id="nombre"
-                            value={nombre}
-                            onChange={(e) => setNombre(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="precio">Precio:</label>
-                        <input
-                            type="number"
-                            id="precio"
-                            value={precio}
-                            onChange={(e) => setPrecio(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="descripcion">Descripción:</label>
-                        <input
-                            type="text"
-                            id="descripcion"
-                            value={descripcion}
-                            onChange={(e) => setDescripcion(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="categoria">Categoría:</label>
-                        <select
-                            id="categoria"
-                            value={categoria}
-                            onChange={(e) => setCategoria(e.target.value)}
-                            required
-                        >
-                            <option value="">Selecciona una categoría</option>
-                            <option value="mujer">Mujer</option>
-                            <option value="hombre">Hombre</option>
-                            <option value="accesorios">Accesorios</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="imagen">Imagen:</label>
-                        <input
-                            type="file"
-                            id="imagen"
-                            onChange={(e) => setImagen(e.target.files[0])}
-                            required
-                        />
-                    </div>
-                    <div className='botones'>
-                        <button type="submit">Agregar nuevo producto</button>
-                        <button type="button" onClick={() => navegacion("/todosLosProductos")}>
-                            Ver todos los productos desde el usuario
-                        </button>
-                        {/*  <button type='submit'>Terminar Edicion</button>*/}
-                    </div>
-                </form>
-            </div>
-        </>
-
+        <div className="cargar-producto-container">
+            {showSuccessMessage && (
+                <div className="success-message">
+                    <p>¡Producto agregado exitosamente!</p>
+                </div>
+            )}
+            {error && (
+                <div className="error-message">
+                    <p>{error}</p>
+                </div>
+            )}
+            <form onSubmit={enviarFormularioProducto} className="form-container">
+                <div>
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="precio">Precio:</label>
+                    <input
+                        type="number"
+                        id="precio"
+                        value={precio}
+                        onChange={(e) => setPrecio(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="descripcion">Descripción:</label>
+                    <input
+                        type="text"
+                        id="descripcion"
+                        value={descripcion}
+                        onChange={(e) => setDescripcion(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label htmlFor="categoria">Categoría:</label>
+                    <select
+                        id="categoria"
+                        value={categoria}
+                        onChange={(e) => setCategoria(e.target.value)}
+                        required
+                    >
+                        <option value="">Selecciona una categoría</option>
+                        <option value="mujer">Mujer</option>
+                        <option value="hombre">Hombre</option>
+                        <option value="accesorios">Accesorios</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="imagen">Imagen:</label>
+                    <input
+                        type="file"
+                        id="imagen"
+                        onChange={(e) => setImagen(e.target.files[0])}
+                        required
+                    />
+                </div>
+                <div className='botones'>
+                    <button type="submit">Agregar nuevo producto</button>
+                    <button type="button" onClick={() => navegacion("/todosLosProductos")}>
+                        Ver todos los productos desde el usuario
+                    </button>
+                </div>
+            </form>
+        </div>
     );
 }
 
