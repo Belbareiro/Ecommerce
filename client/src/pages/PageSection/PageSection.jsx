18aa5582bc531b34cb2b1547ba63afb296a2952e
Belbareiro <mbareiromereles@gmail.com>
2024-08-27 20:01:45 -0400

    cambios

--- client/src/pages/PageSection/PageSection.jsx
+++ client/src/pages/PageSection/PageSection.jsx
@@ -10,10 +10,10 @@ import { Link } from 'react-router-dom';
 
 const PageSection = () => {
   const photos = [
-    { id: 1, src: photo1, title: '20% de descuento en gafas para ti', description: 'Aprovecha nuestro descuento exclusivo en gafas de sol y mejora tu estilo este verano.', buttonText: 'COMPRAR AHORA' },
+    { id: 1, src: photo1, title: 'Mira ya estas gafas especiales para ti', description: 'Resalta tu estilo con nuestrass gafas. Cada  par es una declaración de moda que te hará destacar de la multitud.', buttonText: 'COMPRAR AHORA' },
     { id: 2, src: photo2, title: 'Camisas de manga larga: estilo y comodidad para hombre', description: 'Descubre nuestra colección de camisas de manga larga, perfectas para cualquier ocasión.', buttonText: 'COMPRAR AHORA' },
     { id: 3, src: photo3, title: 'Mira ahora los productos que tenemos para ti', description: 'Explora nuestra amplia gama de productos y encuentra lo que necesitas para tu guardarropa.', buttonText: 'VER MÁS' },
-];
+  ];
   return (
     <div className="page-section">
       <Header />
@@ -28,8 +28,8 @@ const PageSection = () => {
         {photos.map(photo => (
           <div className="photo" key={photo.id}>
             <img src={photo.src} alt={`Foto ${photo.id}`} />
-            <div className='containerTitle'> 
-               <h2 className="title">{photo.title}</h2>
+            <div className='containerTitle'>
+              <h2 className="title">{photo.title}</h2>
             </div>
             <p className="description">{photo.description}</p>
             <button className="button">
